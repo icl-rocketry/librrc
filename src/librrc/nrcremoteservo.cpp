42f5bd6e4276bde4683f3819f52594aa13f70c97
Andrei Paduraru <83088926+Padure3@users.noreply.github.com>
2023-07-25 13:51:02 +0100

    added constructor overload for servo timing in uS, added support for angle limits and started on home position changes

--- src/librrc/nrcremoteservo.cpp
+++ src/librrc/nrcremoteservo.cpp
@@ -7,7 +7,7 @@
 
 void NRCRemoteServo::setup(){
 
-    ledcSetup(_channel,50,16);
+    ledcSetup(_channel,freq,timer_width);
     ledcAttachPin(_gpio,_channel);
     reset(); // send servo to default position
     
@@ -15,12 +15,25 @@ void NRCRemoteServo::setup(){
 
 void NRCRemoteServo::execute_impl(packetptr_t packetptr)
 {
-    
+
     SimpleCommandPacket execute_command(*packetptr);
-    _value = execute_command.arg; // update new position of actuator
 
-    ledcWrite(_channel,angletocounts(execute_command.arg));
+    /*Check if angle value is outside of angle limits. Would also have added checking for the min_angle and max_angle but 
+    rangemap function already has checking for that so there's no point. */
+    if (execute_command.arg > _angl_lim_max)
+    {
+        _value = _angl_lim_max;
+    }
+    else if (execute_command.arg < _angl_lim_min)
+    {
+        _value = _angl_lim_min;
+    }
+    else
+    {
+        _value = execute_command.arg; // update new position of actuator
+    }
 
+    ledcWrite(_channel, angletocounts((uint16_t)_value));
 }
 
 uint16_t NRCRemoteServo::angletocounts(uint16_t angle)
