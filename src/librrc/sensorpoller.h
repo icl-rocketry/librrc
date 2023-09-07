cad641a731c2230a56b04318a271f24710531796
Andrei Paduraru <83088926+Padure3@users.noreply.github.com>
2023-09-08 00:31:01 +0100

    sensor poller got lost in a merge somehow? + fixed polling

---
+++ src/librrc/sensorpoller.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <Arduino.h>
+#include "networksensor.h"
+
+class SensorPoller
+{
+
+    public:
+    SensorPoller(uint16_t poll_interval, NetworkSensor* networksensor):
+        _poll_interval(poll_interval),
+        _networksensor(networksensor)
+        {};
+
+    void setup();
+    void update();
+    float getVal();
+    bool newdata;
+    
+    private:
+
+    const uint16_t _poll_interval;
+    NetworkSensor* _networksensor;
+
+    float sensorvalue;
+
+    const NetworkSensorState* sensorState;
+
+    uint64_t last_time;
+    uint64_t prevNewUpdateTime = 0;
+    uint64_t networkTimeout = 1000;
+};
