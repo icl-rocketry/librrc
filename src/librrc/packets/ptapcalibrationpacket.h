8e4e4df686e2d78b00d994a81f1397ca43a2fa3b
kiran <kd619@ic.ac.uk>
2023-09-28 13:43:46 +0100

    fixed merge issue

---
+++ src/librrc/packets/ptapcalibrationpacket.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <librnp/rnp_packet.h>
+#include <librnp/rnp_serializer.h>
+
+#include <vector>
+
+class PTapCalibrationPacket : public RnpPacket{
+    private:
+    //serializer framework
+        static constexpr auto getSerializer()
+        {
+            auto ret = RnpSerializer(
+                &PTapCalibrationPacket::command,
+                &PTapCalibrationPacket::grad,
+                &PTapCalibrationPacket::c
+            );
+
+            return ret;
+        }
+        
+    public:
+        ~PTapCalibrationPacket();
+
+        PTapCalibrationPacket();
+        /**
+         * @brief Deserialize Packet
+         * 
+         * @param data 
+         */
+        PTapCalibrationPacket(const RnpPacketSerialized& packet);
+        
+        void deserializeBody(std::vector<uint8_t>& buf);
+        /**
+         * @brief Serialize Packet
+         * 
+         * @param buf 
+         */
+        void serialize(std::vector<uint8_t>& buf) override;
+   
+
+        uint8_t command;
+        float c;
+        float grad;
+
+        static constexpr size_t size(){
+            return getSerializer().member_size();
+        }
+
+};
