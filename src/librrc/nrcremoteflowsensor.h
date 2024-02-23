7d139bbd7f0af5aa16f9fb00b4700ef6e9fed205
Soham <ssm422@ic.ac.uk>
2024-02-23 12:04:45 +0000

    Moving Average- replaced divison by coefficient multiplication

--- src/librrc/nrcremoteflowsensor.h
+++ src/librrc/nrcremoteflowsensor.h
@@ -24,7 +24,7 @@ class NRCRemoteFlowSensor : public NRCRemoteSensorBase<NRCRemoteFlowSensor>
         } pcnt_evt_t;
 
         std::deque<float> MovingAvgSamples;
-        int _numSamples = 10;
+        int sampleCoeff = 0.1;
         
 
     public:
@@ -74,7 +74,7 @@ class NRCRemoteFlowSensor : public NRCRemoteSensorBase<NRCRemoteFlowSensor>
             {
                 MovingAverageSum += sample;
             }
-            return MovingAverageSum / _numSamples;
+            return MovingAverageSum*sampleCoeff;
         }
 
 };
