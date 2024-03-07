6c8b70b6c9b2bba120c62cfd3efb8df6e5888ddf
Soham <ssm422@ic.ac.uk>
2024-03-07 21:48:01 +0000

    Revert "PCNT modular code works"
    This reverts commit c29debf57f40c09825a21a152d1650b0e6d0ceec.

--- src/librrc/nrcremoteflowsensor.h
+++ src/librrc/nrcremoteflowsensor.h
@@ -1,67 +1,65 @@
 #pragma once
 
-#include <stdint.h>
+/**
+ * @file FlowSensor.h
+ * @author Soham More
+ * @brief Class to read an analog flow sensor and convert it to a mass flow rate
+ * @version 0.1
+ * @date 2023-01-23
+ */
+
 
 #include <librrc/nrcremotesensorbase.h>
 #include <librnp/rnp_networkmanager.h>
-
-#include <librrc/Helpers/nvsstore.h>
-
+#include <libriccore/platform/esp32/PCNT.h> 
 #include <driver/pcnt.h>
+#include <stdint.h>
 
 class NRCRemoteFlowSensor : public NRCRemoteSensorBase<NRCRemoteFlowSensor>
 {
 
-    private:
-        float _flowRate; 
-        float _AvgflowRate;
-        uint8_t _gpioSig;
-        float _k;
+public:
+   NRCRemoteFlowSensor(RnpNetworkManager &netman, pcnt_unit_t unit, pcnt_channel_t channel, uint8_t _gpioSig , float k):NRCRemoteSensorBase(netman),         
+                                                                    pcnt(unit, channel, _gpioSig),
+                                                                     _k(k){};
 
-        typedef struct
-        {
-            int unit;
-            uint32_t status;
-        } pcnt_evt_t;
 
-        std::deque<float> MovingAvgSamples;
+
+    float getValue() { return _flowRate; };
+    
+
+    void setup()
+    { 
+    
+        pcnt.setup();
+    };
+
+    void update(){
+        _flowSensorCurrTime = esp_timer_get_time();
+        pcnt.update();
+        //get current count 
+        _flowSensorCurrCount = pcnt.getCount();
+        //find difference
+        uint16_t countDiff = (uint16_t)abs((_flowSensorCurrCount - _flowSensorPrevCount));
+        // Serial.println(countDiff);
+        //convert to flow rate and update
+        _flowRate = 1E6*_k*((float)countDiff/(float)(_flowSensorCurrTime - _flowSensorPrevTime));            
+        //update previous count
+         _flowSensorPrevCount = _flowSensorCurrCount;
+        //update previous time
+         _flowSensorPrevTime = _flowSensorCurrTime; 
+    };
+
+private:
+    PCNT pcnt;
+    float _flowRate;
+    float _k;
+  
         
-
-    public:
-        NRCRemoteFlowSensor(RnpNetworkManager &netman, uint8_t _gpioSig, float _k);
-
-        int16_t _flowSensorCurrCount = 0;
-        int16_t _flowSensorPrevCount = 0;
-        int32_t _flowSensorCurrTime = 0;
-        int32_t _flowSensorPrevTime = 0;
-
-       
-        float getValue() { return _AvgflowRate; };
-        void update();
-
-        void setup()
-        {
-            /* Prepare configuration for the PCNT unit */
-            pcnt_config_t pcnt_config = {
-                // Set PCNT input signal and control GPIOs
-                .pulse_gpio_num = _gpioSig,
-                .ctrl_gpio_num = PCNT_PIN_NOT_USED,
-                .pos_mode = PCNT_CHANNEL_EDGE_ACTION_HOLD, 
-                .neg_mode = PCNT_CHANNEL_EDGE_ACTION_INCREASE, 
-                .counter_h_lim = 32767,
-                .counter_l_lim = 0,
-                .unit = PCNT_UNIT_0,
-                .channel = PCNT_CHANNEL_0
-            };
-
-            /* Initialize PCNT unit */
-            pcnt_unit_config(&pcnt_config);
-
-            pcnt_counter_pause(PCNT_UNIT_0);
-            pcnt_counter_clear(PCNT_UNIT_0);
-            pcnt_counter_resume(PCNT_UNIT_0);
-        }
-        
-        int _numSamples = 10;
-        float sampleCoeff = 0.1;
+    int16_t _flowSensorCurrCount;
+    int16_t _flowSensorPrevCount;
+    int32_t _flowSensorCurrTime;
+    int32_t _flowSensorPrevTime;
+    
 };
+
